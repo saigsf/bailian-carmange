ce8041d68b443118d4ba79d7d26ef28864205ae7
saigsf <2270029397@qq.com>
2018-05-08 10:33:19 +0800

    2018/5/8更新

--- js/app/home.js
+++ js/app/home.js
@@ -4,11 +4,11 @@
   });
 
   // 百度地图API功能	
-  // var map = new BMap.Map("map");
+  var map = new BMap.Map("map");
 
-  // map.centerAndZoom(new BMap.Point(104.114129, 37.550339), 5);
-  // map.addControl(new BMap.NavigationControl()); // 添加平移缩放控件
-  // map.enableScrollWheelZoom(); //启用滚轮放大缩小，默认禁用
+  map.centerAndZoom(new BMap.Point(104.114129, 37.550339), 5);
+  map.addControl(new BMap.NavigationControl()); // 添加平移缩放控件
+  map.enableScrollWheelZoom(); //启用滚轮放大缩小，默认禁用
 
   mui.plusReady(function () {
     // 弹出软键盘时自动改变webview的高度
