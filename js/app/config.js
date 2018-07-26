3be5dcf6a113d84ed906890b3af2f77e4934c762
saigsf <2270029397@qq.com>
2018-07-26 10:14:27 +0800

    更新

--- js/app/config.js
+++ js/app/config.js
@@ -1,6 +1,6 @@
 // var BASE_URL_1 = 'http://192.168.0.222:8080/';
-// var BASE_URL_1 = 'http://36.110.56.189:8848/';
-var BASE_URL_1 = 'http://47.98.182.165/';
+var BASE_URL_1 = 'http://36.110.56.189:8848/';
+// var BASE_URL_1 = 'http://47.98.182.165/';
 
 
 /**
